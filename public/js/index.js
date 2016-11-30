2511e37ab0e71a8032cc56970dd3e2c7cdea68e4
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-11-30 21:01:23 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server!');
-
-  socket.emit('createMessage', {
-    from: 'Juan',
-    text: 'This is a message!'
-  });
-
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Derp@derp.pt',
-    text: 'This is a sample message',
-    createdAt: new Date()
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect',  () => {
